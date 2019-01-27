948ca7e153b58e36ee6ee3efa3fd5592bcde62dc
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-27 03:22:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  def initialize(title, author, page_count, genre, turn_page)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+    @turn_page = turn_page
+  end
 
+and then there were none = Title.new("And Then There Were None")
+end
 
+agatha christie = Author.new("Agatha Christie")
+end
+
+272 = Page_Count.new("272")
+end
+
+mystery = Genre.new("Mystery")
+end
+
+flipping the page... wow, you read fast = Turn_Page.new("Flipping the page... Wow, you read fast")
+end
+
+  
