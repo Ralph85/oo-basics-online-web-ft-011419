ebdd5f60c62657bb5f63e1d2e044807e389b028f
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-27 03:48:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,13 @@
 class shoe
-  attr_accessor :
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @title = title
+  end
+
+  def cobble
+    puts "Your shoe is as good as new"
+  end
+
+end
